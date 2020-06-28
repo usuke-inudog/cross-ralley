590e8339a56932a66153b943b229a356c0233c54
usuke-inudog <66010511+usuke-inudog@users.noreply.github.com>
2020-06-28 17:30:21 +0900

    fixed_migrationfile

--- db/migrate/20200619204341_devise_create_users.rb
+++ db/migrate/20200619204341_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nicknamae           null: false
+      t.string :nicknamae,          null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
