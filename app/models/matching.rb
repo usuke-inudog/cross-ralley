5724b1426b9b62075b481b7f2bb70634af149568
usuke-inudog <66010511+usuke-inudog@users.noreply.github.com>
2020-07-30 18:30:49 +0900

    add_matching_function

---
+++ app/models/matching.rb
@@ -0,0 +1,11 @@
+class Matching < ApplicationRecord
+  belongs_to  :host_user, class_name: "User", optional: true, foreign_key: "host_user_id"
+  belongs_to  :guest_user, class_name: "User", optional: true, foreign_key: "guest_user_id"
+
+  validates :host_user,         presence: true
+  validates :guest_user,        presence: true
+  validates :scheduled_date,    presence: true
+  validates :scheduled_time,    presence: true
+  validates :response_deadline, presence: true
+  validates :status,            presence: true
+end
