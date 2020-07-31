cefa2334aac641b4bc55b558d9f0788055e83917
usuke-inudog <66010511+usuke-inudog@users.noreply.github.com>
2020-07-31 10:39:09 +0900

    add_schedule_function

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,17 @@
 class SchedulesController < ApplicationController
+  before_action :set_schedule, only: [:index]
+  
   def index
+  end
+
+  private
+  def set_schedule
+    @schedules = Matching.where(host_user_id: current_user.id).or(Matching.where(guest_user_id: current_user.id))
+    @unapproved = @schedules.where(guest_user_id: current_user.id).where(status: "申込中").where('scheduled_date>= ?', Date.today)
+    @applying = @schedules.where(host_user_id: current_user.id).where(status: "申込中").where('scheduled_date>= ?', Date.today)
+    @approved = @schedules.where(status: "承認済").where('scheduled_date>= ?',Date.today)
+    @unrating = @schedules.where('scheduled_date >= ?', Date.today).where.not(status: "完了").where.not(status:"申込中")
+    @completed = @schedules.where(status: "完了")
+    @expired = @schedules.where(host_user_id: current_user.id).where(status: "申込中").where('scheduled_date< ?', Date.today)
   end
 end
